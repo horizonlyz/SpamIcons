8b2248c696664b357b0d5fc05f442a047a671736
hzy <44131738+horizonlyz@users.noreply.github.com>
2023-04-11 02:27:18 +0700

    Note for the shield icon

--- SpamIcons/main.cpp
+++ SpamIcons/main.cpp
@@ -8,7 +8,9 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR nCmdLine,
 	while (true) {
 		x = rand() % GetSystemMetrics(SM_CXSCREEN);
 		y = rand() % GetSystemMetrics(SM_CYSCREEN);
-		LPCWSTR icons[] = { IDI_APPLICATION, IDI_ASTERISK, IDI_EXCLAMATION, IDI_HAND, IDI_QUESTION, IDI_SHIELD, IDI_WINLOGO };
+		LPCWSTR icons[] = { 
+			IDI_APPLICATION, IDI_ASTERISK, IDI_EXCLAMATION, IDI_HAND, IDI_QUESTION, IDI_SHIELD, IDI_WINLOGO
+			}; // The shield icon wouldn't appears on Windows XP
 		LPCWSTR ChoosenIcon = icons[rand() % ARRAYSIZE(icons)];
 		DrawIcon(hDc, x, y, LoadIcon(NULL, ChoosenIcon));
 		Sleep(40);
