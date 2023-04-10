5b8f02787013c79fff3547ddf520d56db6efdb90
hzy <44131738+horizonlyz@users.noreply.github.com>
2023-04-10 13:31:45 +0700

    Add files via upload

---
+++ SpamIcons/main.cpp
@@ -0,0 +1,16 @@
+typedef struct IUnknown IUnknown;
+#include <Windows.h>
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR nCmdLine, int nCmdShow) {
+	HDC hDc = GetWindowDC(GetDesktopWindow());
+	int x = rand() % GetSystemMetrics(SM_CXSCREEN);
+	int y = rand() % GetSystemMetrics(SM_CYSCREEN);
+	while (true) {
+		x = rand() % GetSystemMetrics(SM_CXSCREEN);
+		y = rand() % GetSystemMetrics(SM_CYSCREEN);
+		LPCWSTR icons[] = { IDI_APPLICATION, IDI_ASTERISK, IDI_EXCLAMATION, IDI_HAND, IDI_QUESTION, IDI_WINLOGO };
+		LPCWSTR ChoosenIcon = icons[rand() % ARRAYSIZE(icons)];
+		DrawIcon(hDc, x, y, LoadIcon(NULL, ChoosenIcon));
+		Sleep(40);
+	}
+}
