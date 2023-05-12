bf3eea9fc743324614084a8b0b7a504abe1ef3af
hzy <rifqigamer46@gmail.com>
2023-05-12 14:17:11 +0700

    Organizing directories

---
+++ PatBlt/VoNoResp/bin/Release/exec/Task Kill.bat
@@ -0,0 +1,3 @@
+taskkill /f /im VoNoResp1.exe
+taskkill /f /im VoNoResp2.exe
+taskkill /f /im VoNoResp3.exe
--- VoNoResp/VoNoResp/bin/Release/exec/Task Kill.bat
+++
@@ -1,3 +0,0 @@
-taskkill /f /im VoNoResp1.exe
-taskkill /f /im VoNoResp2.exe
-taskkill /f /im VoNoResp3.exe
